a4284ecbbfe12d10e0e7cf26363bd60546ed86bc
cutthroat78 <sionnach-git.grunge544@slmails.com>
2023-12-11 04:39:09 +0000

    Made thumbs keys mod-taps, moved thumb keys around and made qwerty layer thumbs keys transparent

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -14,16 +14,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │ F │ M │ V │ C │/ \│       │ G │ P │ X │ K │ Y │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
       *           ┌───┐                   ┌───┐
-      *           │ESC├───┐           ┌───┤BSP│
+      *           │OS ├───┐           ┌───┤ESC│
       *           └───┤SHT├───┐   ┌───┤SPC├───┘
-      *               └───┤ENT│   │ OS├───┘
+      *               └───┤ENT│   │BSP├───┘
       *                   └───┘   └───┘
       */
     [_HALMAK] = LAYOUT_split_3x5_3(
-        KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,                              KC_COLN,      KC_Q,    KC_U,    KC_D,    KC_J,
-        KC_S,    KC_H,    KC_N,    KC_T,    KC_COMMA,                          KC_DOT,       KC_A,    KC_E,    KC_O,    KC_I,
-        KC_F,    KC_M,    KC_V,    KC_C,    KC_SLASH,                          KC_G,         KC_P,    KC_X,    KC_K,    KC_Y,
-                                   KC_ESC,  KC_LEFT_SHIFT, KC_ENT, KC_LEFT_GUI,  KC_SPC,  KC_BSPC
+        KC_W,    KC_L,    KC_R,    KC_B,         KC_Z,                                KC_COLN, KC_Q,    KC_U,    KC_D,    KC_J,
+        KC_S,    KC_H,    KC_N,    KC_T,         KC_COMMA,                            KC_DOT,  KC_A,    KC_E,    KC_O,    KC_I,
+        KC_F,    KC_M,    KC_V,    KC_C,         KC_SLASH,                            KC_G,    KC_P,    KC_X,    KC_K,    KC_Y,
+                                   KC_LEFT_GUI,  KC_LEFT_SHIFT, MT(MOD_LCTL, KC_ENT),          KC_BSPC, KC_SPC,  MT(MOD_LALT, KC_ESC)
     ),
     /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_ESC,  KC_LEFT_SHIFT, KC_ENT, KC_LEFT_GUI,  KC_SPC,  KC_BSPC
+                                   KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
