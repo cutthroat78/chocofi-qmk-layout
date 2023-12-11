7871ebc4d58ff5868db7b82d73a4d161d593161c
cutthroat78 <sionnach-git.grunge544@slmails.com>
2023-12-11 03:50:49 +0000

    Combined backspace and delete and changed thumb keys on Halmak

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [_HALMAK] = LAYOUT_split_3x5_3(
-        KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,                              KC_COLN,    KC_Q,    KC_U,    KC_D,    KC_J,
-        KC_S,    KC_H,    KC_N,    KC_T,    KC_COMMA,                          KC_DOT,     KC_A,    KC_E,    KC_O,    KC_I,
-        KC_F,    KC_M,    KC_V,    KC_C,    KC_SLASH,                          KC_G,       KC_P,    KC_X,    KC_K,    KC_Y,
-                                   KC_BSPC, KC_LEFT_SHIFT, KC_DELETE,          KC_LEFT_GUI,  KC_SPC,  KC_ENT
+        KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,                              KC_COLN,      KC_Q,    KC_U,    KC_D,    KC_J,
+        KC_S,    KC_H,    KC_N,    KC_T,    KC_COMMA,                          KC_DOT,       KC_A,    KC_E,    KC_O,    KC_I,
+        KC_F,    KC_M,    KC_V,    KC_C,    KC_SLASH,                          KC_G,         KC_P,    KC_X,    KC_K,    KC_Y,
+                                   KC_ESC,  KC_LEFT_SHIFT, KC_ENT, KC_LEFT_GUI,  KC_SPC,  KC_BSPC
     ),
     /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
       *           ┌───┐                   ┌───┐
-      *           │BSP├───┐           ┌───┤ENT│
+      *           │ESC├───┐           ┌───┤BSP│
       *           └───┤SHT├───┐   ┌───┤SPC├───┘
-      *               └───┤DEL│   │ OS├───┘
+      *               └───┤ENT│   │ OS├───┘
       *                   └───┘   └───┘
       */
     [_QWERTY] = LAYOUT_split_3x5_3(
@@ -51,6 +51,7 @@ const key_override_t colon_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC
 const key_override_t comma_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMMA, KC_LPRN, ~0);
 const key_override_t dot_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT, KC_RPRN, ~0);
 const key_override_t slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_BACKSLASH, ~0);
+const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~0);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -58,6 +59,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	&comma_key_override,
 	&dot_key_override,
 	&slash_key_override,
+	&delete_key_override,
 	NULL // Null terminate the array of overrides!
 };
 
