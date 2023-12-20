adf6e98a25b2cce242c73bd5e7f0996dd46ab799
cutthroat78 <sionnach-git.grunge544@slmails.com>
2023-12-20 22:18:03 +0000

    Added numbers layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,6 +4,7 @@
 #define _HALMAK 0
 #define _QWERTY 1
 #define _NAV 2
+#define _NUMBERS 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -55,6 +56,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT,   KC_UP,      KC_DOWN,      KC_RIGHT, KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN,      KC_UP,      KC_RIGHT,
         KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
                                              KC_TRNS,  KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │       │ + │ 4 │ 5 │ 6 │/ *│
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │       │ - │ 1 │ 2 │ 3 │ . │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      */
+    [_NUMBERS] = LAYOUT_split_3x5_3(
+        KC_HOME,   KC_PAGE_UP, KC_PAGE_DOWN, KC_END,   KC_TRNS,                               KC_TRNS, KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP, KC_END,
+        KC_LEFT,   KC_UP,      KC_DOWN,      KC_RIGHT, KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN,      KC_UP,      KC_RIGHT,
+        KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
+                                             KC_TRNS,  KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
@@ -62,7 +78,8 @@ const key_override_t colon_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC
 const key_override_t comma_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMMA, KC_LPRN, ~_HALMAK);
 const key_override_t dot_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT, KC_RPRN, ~_HALMAK);
 const key_override_t slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_BACKSLASH, ~_HALMAK);
-const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_NAV | ~_QWERTY);
+const key_override_t numbers_slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_KP_ASTERISK, ~_NUMBERS);
+const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_NAV | ~_QWERTY | ~_NUMBERS);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -70,24 +87,27 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	&comma_key_override,
 	&dot_key_override,
 	&slash_key_override,
+	&numbers_slash_key_override,
 	&delete_key_override,
 	NULL // Null terminate the array of overrides!
 };
 
 const uint16_t PROGMEM halmak_layer_combo[] = {KC_T, KC_A, COMBO_END};
 const uint16_t PROGMEM nav_layer_combo[] = {KC_T, KC_E, COMBO_END};
-const uint16_t PROGMEM qwerty_layer_combo[] = {KC_T, KC_O, COMBO_END};
+const uint16_t PROGMEM numbers_layer_combo[] = {KC_T, KC_O, COMBO_END};
+const uint16_t PROGMEM qwerty_layer_combo[] = {KC_T, KC_Q, COMBO_END};
 
-const uint16_t PROGMEM osl_halmak_layer_combo[] = {KC_T, KC_N, KC_A, COMBO_END};
 const uint16_t PROGMEM osl_nav_layer_combo[] = {KC_T, KC_N, KC_E, COMBO_END};
-const uint16_t PROGMEM osl_qwerty_layer_combo[] = {KC_T, KC_N, KC_O, COMBO_END};
+const uint16_t PROGMEM osl_numbers_layer_combo[] = {KC_T, KC_N, KC_O, COMBO_END};
+const uint16_t PROGMEM osl_qwerty_layer_combo[] = {KC_T, KC_N, KC_Q, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(halmak_layer_combo, TO(_HALMAK)),
     COMBO(nav_layer_combo, TO(_NAV)),
+    COMBO(numbers_layer_combo, TO(_NUMBERS)),
     COMBO(qwerty_layer_combo, TO(_QWERTY)),
-    COMBO(osl_halmak_layer_combo, OSL(_HALMAK)),
     COMBO(osl_nav_layer_combo, OSL(_NAV)),
+    COMBO(osl_numbers_layer_combo, OSL(_NUMBERS)),
     COMBO(osl_qwerty_layer_combo, OSL(_QWERTY)),
 };
 
