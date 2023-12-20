1d208c3c7f1592ab90e1eb16789dabedd72191de
cutthroat78 <sionnach-git.grunge544@slmails.com>
2023-12-20 12:30:38 +0000

    Added nav layer and OSL versions of every layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,6 +3,7 @@
 
 #define _HALMAK 0
 #define _QWERTY 1
+#define _NAV 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -39,6 +40,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │END│PGU│PGD│HME│   │       │   │HME│PGD│PGU│END│
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │➡  │⬆  │ ⬇ │⬅  │   │       │   │ ⬅ │ ⬇ │ ⬆ │ ➡ │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │       │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      */
+    [_NAV] = LAYOUT_split_3x5_3(
+        KC_END,    KC_PAGE_UP, KC_PAGE_DOWN, KC_HOME, KC_TRNS,                               KC_TRNS, KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP, KC_END,
+        KC_RIGHT,  KC_UP,      KC_DOWN,      KC_LEFT, KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN,      KC_UP,      KC_RIGHT,
+        KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
+                                             KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
@@ -46,7 +62,7 @@ const key_override_t colon_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC
 const key_override_t comma_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMMA, KC_LPRN, ~_HALMAK);
 const key_override_t dot_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT, KC_RPRN, ~_HALMAK);
 const key_override_t slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_BACKSLASH, ~_HALMAK);
-const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_QWERTY);
+const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_NAV | ~_QWERTY);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -59,9 +75,19 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 };
 
 const uint16_t PROGMEM halmak_layer_combo[] = {KC_T, KC_A, COMBO_END};
-const uint16_t PROGMEM qwerty_layer_combo[] = {KC_T, KC_E, COMBO_END};
+const uint16_t PROGMEM nav_layer_combo[] = {KC_T, KC_E, COMBO_END};
+const uint16_t PROGMEM qwerty_layer_combo[] = {KC_T, KC_O, COMBO_END};
+
+const uint16_t PROGMEM osl_halmak_layer_combo[] = {KC_T, KC_N, KC_A, COMBO_END};
+const uint16_t PROGMEM osl_nav_layer_combo[] = {KC_T, KC_N, KC_E, COMBO_END};
+const uint16_t PROGMEM osl_qwerty_layer_combo[] = {KC_T, KC_N, KC_O, COMBO_END};
+
 combo_t key_combos[] = {
     COMBO(halmak_layer_combo, TO(_HALMAK)),
+    COMBO(nav_layer_combo, TO(_NAV)),
     COMBO(qwerty_layer_combo, TO(_QWERTY)),
+    COMBO(osl_halmak_layer_combo, OSL(_HALMAK)),
+    COMBO(osl_nav_layer_combo, OSL(_NAV)),
+    COMBO(osl_qwerty_layer_combo, OSL(_QWERTY)),
 };
 
