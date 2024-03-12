ee8619f2f58762259a9ad8fb9d913db7d59dba83
cutthroat78 <sionnach-git.grunge544@slmails.com>
2024-03-12 19:45:58 +0000

    Added a lot of different changes to layers

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,7 +4,8 @@
 #define _HALMAK 0
 #define _QWERTY 1
 #define _NAV 2
-#define _ANKI 3
+#define _NUM 3
+#define _ANKI 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -23,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [_HALMAK] = LAYOUT_split_3x5_3(
         KC_W,    KC_L,    KC_R,    KC_B,         KC_Z,                                KC_COLN, KC_Q,    KC_U,    KC_D,    KC_J,
-        KC_S,    KC_H,    KC_N,    KC_T,         KC_COMMA,                            KC_DOT,  KC_A,    KC_E,    KC_O,    KC_I,
-        KC_F,    KC_M,    KC_V,    KC_C,         KC_SLASH,                            KC_G,    KC_P,    KC_X,    KC_K,    KC_Y,
+        KC_S,    KC_H,    KC_N,    KC_T,         MT(MO(_NAV), KC_COMMA),                            KC_DOT,  KC_A,    KC_E,    KC_O,    KC_I,
+        KC_F,    KC_M,    KC_V,    KC_C,         MT(MO(_NUM), KC_SLASH),                            KC_G,    KC_P,    KC_X,    KC_K,    KC_Y,
                                    KC_LEFT_GUI,  KC_LEFT_SHIFT, MT(MOD_LCTL, KC_ENT),          KC_BSPC, KC_SPC,  MT(MOD_LALT, KC_ESC)
     ),
     /*
@@ -66,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │   │   │   │   │   │       │ - │ 1 │ 2 │ 3 │ . │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
       */
-    [_NUMBERS] = LAYOUT_split_3x5_3(
+    [_NUM] = LAYOUT_split_3x5_3(
         KC_1,      KC_2,       KC_3,         KC_4,     KC_5,                                  KC_6,    KC_7,    KC_8, KC_9, KC_0,
         KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_PPLS, KC_4,    KC_5, KC_6, KC_PSLS,
         KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_MINS, KC_1,    KC_2, KC_3, KC_DOT,
@@ -90,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,   KC_E,    KC_B,      KC_D,  KC_TRNS,                                            KC_TRNS, KC_D, KC_B,      KC_E,    KC_A,
         KC_4,   KC_3,    KC_2,      KC_1,  KC_Y,                                               KC_Y, KC_1, KC_2,      KC_3,    KC_4,
         KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_SLASH,                               KC_SLASH, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
-                                             KC_TRNS,  KC_TRNS, LCTL(z),             LCTL(z), KC_TRNS, KC_TRNS
+                                             KC_TRNS,  KC_SPC, LCTL(KC_Z),             LCTL(KC_Z), KC_SPC, KC_TRNS
     )
 };
 
@@ -98,9 +99,9 @@ const key_override_t colon_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC
 const key_override_t comma_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMMA, KC_LPRN, ~_HALMAK);
 const key_override_t dot_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT, KC_RPRN, ~_HALMAK);
 const key_override_t slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_SLASH, KC_BACKSLASH, ~_HALMAK);
-const key_override_t numbers_slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_PSLS, KC_KP_ASTERISK, ~_NUMBERS);
-const key_override_t numbers_plus_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_PPLS, KC_EQUAL, ~_NUMBERS);
-const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_NAV | ~_QWERTY | ~_NUMBERS);
+const key_override_t numbers_slash_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_PSLS, KC_KP_ASTERISK, ~_NUM);
+const key_override_t numbers_plus_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_PPLS, KC_EQUAL, ~_NUM);
+const key_override_t delete_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE, ~_HALMAK | ~_NAV | ~_QWERTY | ~_NUM);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -114,22 +115,17 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	NULL // Null terminate the array of overrides!
 };
 
-const uint16_t PROGMEM halmak_layer_combo[] = {KC_T, KC_A, COMBO_END};
-const uint16_t PROGMEM nav_layer_combo[] = {KC_T, KC_E, COMBO_END};
-const uint16_t PROGMEM numbers_layer_combo[] = {KC_T, KC_O, COMBO_END};
-const uint16_t PROGMEM qwerty_layer_combo[] = {KC_T, KC_Q, COMBO_END};
+const uint16_t PROGMEM nav_layer_combo[] = {KC_COLN, KC_S, COMBO_END};
+const uint16_t PROGMEM numbers_layer_combo[] = {KC_COLN, KC_W, COMBO_END};
 
-const uint16_t PROGMEM osl_nav_layer_combo[] = {KC_T, KC_N, KC_E, COMBO_END};
-const uint16_t PROGMEM osl_numbers_layer_combo[] = {KC_T, KC_N, KC_O, COMBO_END};
-const uint16_t PROGMEM osl_qwerty_layer_combo[] = {KC_T, KC_N, KC_Q, COMBO_END};
+const uint16_t PROGMEM halmak_layer_combo[] = {KC_DOT, KC_S, COMBO_END};
+const uint16_t PROGMEM qwerty_layer_combo[] = {KC_DOT, KC_W, COMBO_END};
+const uint16_t PROGMEM anki_layer_combo[] = {KC_DOT, KC_F, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(halmak_layer_combo, TO(_HALMAK)),
     COMBO(nav_layer_combo, TO(_NAV)),
-    COMBO(numbers_layer_combo, TO(_NUMBERS)),
+    COMBO(numbers_layer_combo, TO(_NUM)),
+    COMBO(halmak_layer_combo, TO(_HALMAK)),
     COMBO(qwerty_layer_combo, TO(_QWERTY)),
-    COMBO(osl_nav_layer_combo, OSL(_NAV)),
-    COMBO(osl_numbers_layer_combo, OSL(_NUMBERS)),
-    COMBO(osl_qwerty_layer_combo, OSL(_QWERTY)),
+    COMBO(anki_layer_combo, TO(_ANKI)),
 };
-
