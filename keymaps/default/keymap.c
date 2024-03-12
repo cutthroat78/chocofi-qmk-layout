c444d7b11c1149ecd0389bfdcf67cb3d9229e6c4
cutthroat78 <sionnach-git.grunge544@slmails.com>
2024-03-12 20:08:12 +0000

    Moved keys around on num layer to make pressing 0 easier

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -60,16 +60,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │
+      * │   │   │   │   │   │       │+ =│ 7 │ 8 │ 9 │   │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │   │   │   │   │   │       │+ =│ 4 │ 5 │ 6 │/ *│
+      * │   │   │   │   │   │       │ 0 │ 4 │ 5 │ 6 │/ *│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │   │   │   │   │   │       │ - │ 1 │ 2 │ 3 │ . │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
       */
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_1,      KC_2,       KC_3,         KC_4,     KC_5,                                  KC_6,    KC_7,    KC_8, KC_9, KC_0,
-        KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_PPLS, KC_4,    KC_5, KC_6, KC_PSLS,
+        KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_PPLS,    KC_7,    KC_8, KC_9, KC_0,
+        KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_0, KC_4,    KC_5, KC_6, KC_PSLS,
         KC_TRNS,   KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,                               KC_MINS, KC_1,    KC_2, KC_3, KC_DOT,
                                              KC_TRNS,  KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
     ),
